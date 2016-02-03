1d7b30dace7203a45e3d69963de50da2e3a58acc
Srivatsan Venkatesan <vsrivatsan89@gmail.com>
2016-02-02 23:00:34 -0500

    Add ?completed filter

--- server.js
+++ server.js
@@ -17,7 +17,16 @@ app.get('/', function (req, res){
 
 app.get('/todos',function (req, res)
 {
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true'){
+		filteredTodos = _.where(filteredTodos,{completed: true});
+	}else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
+		filteredTodos = _.where(filteredTodos,{completed: false});
+	}
+
+	res.json(filteredTodos);
 });
 
 app.get('/todos/:id', function (req,res){
@@ -67,6 +76,36 @@ app.post('/todos', function (req,res){
 	todos.push(body);
 	res.json(body);
 	}
+});
+
+app.put('/todos/:id', function (req,res){
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttributes.completed = body.completed;
+	}else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0)
+	{
+		validAttributes.description = body.description;
+	}else if (body.hasOwnProperty('description')){
+		return res.status(400).send();
+	}
+	var todoID = parseInt(req.params.id);
+	var matchedTodo = _.findWhere(todos, {id: todoID});
+
+	if (matchedTodo){
+		todos = _.without(todos,matchedTodo);
+		_.extend(matchedTodo, validAttributes);
+		todos.push(matchedTodo);
+		res.json(matchedTodo);
+	}else {
+		return res.status(404).send();
+	}
+
 });
 
 app.listen(PORT,function () {
