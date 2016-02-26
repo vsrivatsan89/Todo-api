b3fde40d9eb34c8c7c6d794a33a78dba406ec790
Srivatsan Venkatesan <vsrivatsan89@gmail.com>
2016-02-26 00:44:52 -0500

    PUT /:id Sequelized

--- server.js
+++ server.js
@@ -103,33 +103,31 @@ app.post('/todos', function(req, res) {
 });
 
 app.put('/todos/:id', function(req, res) {
+	var todoId = parseInt(req.params.id, 10);
 	var body = _.pick(req.body, 'description', 'completed');
-	var validAttributes = {};
+	var attributes = {};
 
-	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-		validAttributes.completed = body.completed;
-	} else if (body.hasOwnProperty('completed')) {
-		return res.status(400).send();
+	if (body.hasOwnProperty('completed')) {
+		attributes.completed = body.completed;
 	}
 
-	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-		validAttributes.description = body.description;
-	} else if (body.hasOwnProperty('description')) {
-		return res.status(400).send();
+	if (body.hasOwnProperty('description')) {
+		attributes.description = body.description;
 	}
-	var todoID = parseInt(req.params.id);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoID
+
+	db.todo.findById(todoId).then(function(todo) {
+		if (todo) {
+			todo.update(attributes).then(function(todo) {
+				res.json(todo.toJSON());
+			}, function(e) {
+				res.status(400).json(e);
+			});
+		} else {
+			res.status(404).send();
+		}
+	}, function() {
+		res.status(500).send();
 	});
-
-	if (matchedTodo) {
-		todos = _.without(todos, matchedTodo);
-		_.extend(matchedTodo, validAttributes);
-		todos.push(matchedTodo);
-		res.json(matchedTodo);
-	} else {
-		return res.status(404).send();
-	}
 
 });
 
