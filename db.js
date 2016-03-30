b4868f1ee84a4f802c7f22112345f3cc5f7b9995
Srivatsan Venkatesan <srivatsan@oc7366145033.ibm.com>
2016-03-30 17:31:39 -0400

    Added test comment

--- db.js
+++ db.js
@@ -12,7 +12,6 @@ if(env === 'production'){
 });
 
 }
-//tet
 
 var db = {};
 
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//test comment
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
