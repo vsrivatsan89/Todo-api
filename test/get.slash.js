83fc6e7d9bc53a641e06a0473d5bc2d9f7ffb6e8
Srivatsan Venkatesan <vsrivatsan89@gmail.com>
2017-05-27 00:19:26 -0500

    Implemented unit test for GET /

--- test/get.slash.js
+++ test/get.slash.js
@@ -9,7 +9,10 @@ describe('/', () => {
 	it('responds to / ', function testSlash(done) {
 	chai.request(server).get('/').end(function(err , res){
 
+		//response status code should be 200
+		//response text should be "TODO API Root"
 		res.should.have.status(200);
+		res.text.should.equal('TODO API Root');
 		done();
 
 
