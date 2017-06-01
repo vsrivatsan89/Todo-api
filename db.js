46c96249c416f279ea19513e0d4ccd561affca3f
Srivatsan Venkatesan <venkates@us.ibm.com>
2017-06-01 16:09:49 -0400

    Disable logging with seqeuelize

--- db.js
+++ db.js
@@ -3,18 +3,21 @@ var env = process.env.NODE_ENV || 'development';
 var sequelize;
 if (env === 'production') {
     sequelize = new Sequelize(process.env.DATABASE_URL, {
-        dialect: 'postgres'
+        dialect: 'postgres',
+        logging: false
     });
 } else if (env === 'testing') {
     sequelize = new Sequelize(undefined, undefined, undefined, {
         'dialect': 'sqlite',
-        'storage': __dirname + '/data/testing-todo-api.sqlite'
+        'storage': __dirname + '/data/testing-todo-api.sqlite',
+        logging: false
     });
 
 } else {
     sequelize = new Sequelize(undefined, undefined, undefined, {
         'dialect': 'sqlite',
-        'storage': __dirname + '/data/dev-todo-api.sqlite'
+        'storage': __dirname + '/data/dev-todo-api.sqlite',
+        logging: false
     });
 
 }
